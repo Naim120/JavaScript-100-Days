ddd914fca29bba9938b6ef89db875eaf9a8e8f9a
Cielo <macielo.tablate@gmail.com>
2023-03-06 14:05:30 +0100

    day02

---
+++ 02-RANDOMNUMGEN/script.js
@@ -0,0 +1,16 @@
+const randomNum = document.querySelector('.rand_num');
+const genBtn = document.querySelector('.gen_button');
+
+genBtn.addEventListener('click', () => {
+  const randomNumber = Math.floor(Math.random() * (20 - 1) + 1);
+  randomNum.textContent = randomNumber;
+});
+
+//Alternative for above
+
+// const generateRandomNumber = () => {
+//   const randomNumber = Math.floor(Math.random() * (20 + 1) + 1);
+//   randomNum.textContent = randomNumber;
+// };
+
+// genBtn.addEventListener('click', generateRandomNumber);
