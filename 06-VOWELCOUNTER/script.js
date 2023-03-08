fc75bbcf2a93e3301f7d1a9a814987103d5abd66
Cielo <macielo.tablate@gmail.com>
2023-03-08 18:08:16 +0100

    day06

---
+++ 06-VOWELCOUNTER/script.js
@@ -0,0 +1,28 @@
+// Select the DOM Elements
+const wordToCheck = document.querySelector('.text_check');
+const checkBtn = document.querySelector('.check_btn');
+const resultPara = document.querySelector('.result_text');
+
+// Add event listener
+checkBtn.addEventListener('click', countVowel);
+
+// Define event listener function
+function countVowel() {
+  const strToCheck = wordToCheck.value;
+  let vowelCount = 0;
+
+  for (i = 0; i < strToCheck.length; i++) {
+    let letter = strToCheck[i];
+    const regex = /[aeiouAEIOU]/g;
+    // const regex = /[B-DF-HJ-NP-TV-Zb-df-hj-np-tv-z]/g   ; // consonant
+    if (letter.match(regex)) {
+      vowelCount++;
+    }
+  }
+
+  vowelWord = vowelCount > 1 ? 'vowels' : 'vowel';
+  resultPara.textContent =
+    strToCheck === ''
+      ? ''
+      : (resultPara.textContent = `${strToCheck} has ${vowelCount} ${vowelWord}.`);
+}
