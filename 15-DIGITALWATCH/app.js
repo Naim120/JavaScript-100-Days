3bcc4b3c9a48c2eff8afd7ec5e4f4e5cb02962ab
Cielo <macielo.tablate@gmail.com>
2023-03-21 14:10:18 +0100

    update design

--- 15-DIGITALWATCH/app.js
+++ 15-DIGITALWATCH/app.js
@@ -5,7 +5,7 @@ function updateTime() {
   let hours = now.getHours();
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
-  hours = hours % 12; // converts in 12 Hr Format
+//   hours = hours % 12; // converts in 12 Hr Format
   hours = hours ? hours : 12; // hours 0 should be 12
   hours = hours < 10 ? `0${hours}` : hours;
   minutes = minutes < 10 ? `0${minutes}` : minutes;
